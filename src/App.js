f59fbc84e85d1f3dd993bd31173704c444041556
Clint Mullins <clintfmullins@gmail.com>
2018-11-17 13:44:44 -0800

    testing changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Shit's Working
+            This might be under construction
           </p>
           <a
             className="App-link"
