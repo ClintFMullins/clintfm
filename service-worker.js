726de2789471dd8a02f0c35d770f77d89c4d9b0d
Clint Mullins <clintfmullins@gmail.com>
2018-11-21 17:19:51 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/clintfm/precache-manifest.4cbf7a537d8df38b51b5280d1128edaa.js"
+  "/clintfm/precache-manifest.7e8a4e0449cdfc9a8b917052e46e5963.js"
 );
 
 workbox.clientsClaim();
