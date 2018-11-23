d3eab339e6c032fd1a367934379e398383d483fe
Clint Mullins <clintfmullins@gmail.com>
2018-11-23 09:47:47 -0800

    better mobile styles

--- src/pages/play/component.js
+++ src/pages/play/component.js
@@ -7,26 +7,29 @@ import './styles.css';
 export function Play() {
   return (
     <div className="play">
-      <div className="showcase-wrapper">
-        <Showcase
-          title="Time++"
-          description="Starting now, and so on."
-          link={`/projects/current-time`}
-          hue={3}
-        >
-          <TimeWidget size={150} isRound={true} />
-        </Showcase>
-      </div>
+      <h1>Projects</h1>
+      <div className="play-projects">
+        <div className="showcase-wrapper">
+          <Showcase
+            title="Time++"
+            description="Starting now, and so on."
+            link={`/projects/current-time`}
+            hue={3}
+          >
+            <TimeWidget size={150} isRound={true} />
+          </Showcase>
+        </div>
 
-      <div className="showcase-wrapper">
-        <Showcase
-          title="Call To Action"
-          description="We want you to click this one."
-          link={`/projects/cta`}
-          hue={3}
-        >
-          <PreviewCTA />
-        </Showcase>
+        <div className="showcase-wrapper">
+          <Showcase
+            title="CTA"
+            description="We want you to click this one."
+            link={`/projects/cta`}
+            hue={3}
+          >
+            <PreviewCTA />
+          </Showcase>
+        </div>
       </div>
     </div>
   );
