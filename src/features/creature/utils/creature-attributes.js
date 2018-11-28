7d40a99892c7c7c0d4885ba37e29f66c1bf5fcdd
Clint Mullins <clintfmullins@gmail.com>
2018-11-28 00:18:43 -0500

    creature-finder-with-shit-mobile-ux

---
+++ src/features/creature/utils/creature-attributes.js
@@ -0,0 +1,63 @@
+import { randomInRange } from "./random";
+
+
+// This needs to be completely redone to make sense.
+export function getCreatureAttributes(creatureData) {
+  const { body, mouth, eyes, misc } = creatureData;
+
+  const factorMouthBig = (mouth.sizeX + mouth.sizeY) * 4;
+  const factorMouthSquare = (mouth.topLeftRadius + mouth.topRightRadius + mouth.bottomLeftRadius + mouth.bottomRightRadius) / 4;
+  const aggression = Math.floor(factorMouthBig - factorMouthSquare);
+
+  const factorBodyBig = (body.width * 100) + (body.height * 100);
+  const factorBodySquare = body.radius * 4;
+  const defense = Math.floor(factorBodyBig + factorBodySquare);
+
+  const factorEyeBig = (eyes.sizeX + eyes.sizeY) * 4;
+  const charm = Math.floor(400 - factorEyeBig - factorMouthSquare);
+
+  const speed = Math.floor(200 + factorEyeBig - factorBodyBig);
+
+  const factorEyeForwardness = eyes.leftX + eyes.leftY + eyes.rightX + eyes.rightY;
+  const intimidate = Math.floor(300 - factorEyeForwardness);
+
+  const volume = Math.floor(factorMouthBig + eyes.pupilX + eyes.pupilY);
+
+  const factorEyePupilSize = eyes.pupilSizeX + eyes.pupilSizeY;
+  const intuition = Math.floor(factorEyeForwardness - factorEyePupilSize);
+
+  return {
+    type: getClosestType(misc.hue),
+    aggression, // mouth big // mouth square
+    defense, // body big // body square
+    charm, // eye small // mouth round
+    speed, // body small // eyes big
+    intimidate, // eyes high // eyes forward
+    volume, // mouth big // pupil large
+    intuition, // eyes low // pupil small
+  }
+}
+
+// If I ever make a battle system,
+// this is not good enough. 
+//0 - red        - Fire
+// 60 - yellow    - Sun
+// 120 - green    - Forest
+// 180 - cyan     - Wind
+// 240 - deep blue  - Water
+// 300 purple      - Spirit
+function getClosestType(hue) {
+  if (hue >= 30 && hue < 90) {
+    return 'Sun';
+  } else if (hue >= 90 && hue < 150) {
+    return 'Forest';
+  } else if (hue >= 150 && hue < 210) {
+    return 'Wind';
+  } else if (hue >= 210 && hue < 270) {
+    return 'Water';
+  } else if (hue >= 270 && hue < 330) {
+    return 'Spirit';
+  } else {
+    return 'Fire';
+  }
+}
--- src/pages/projects/components/creature-presentation/component.js
+++ src/pages/projects/components/creature-presentation/component.js
@@ -1,18 +1,53 @@
 import React, {useState} from 'react';
 import { Creature } from '../../../../features/creature/component';
 import { generateCreatureData } from '../../../../features/creature/utils/creature-generation';
+import { getCreatureAttributes } from '../../../../features/creature/utils/creature-attributes';
+import { TimeWidget } from '../../../../features/time-widget/component';
 import './styles.css';
 
+function getNewCreature() {
+  const data = generateCreatureData();
+  const attributes = getCreatureAttributes(data);
+
+  return { data, attributes };
+}
+
+function Attribute(props) {
+  return (
+    <div>
+      <span className="attribute-bold">{props.label}</span>: {props.value}
+    </div>
+  );
+}
+
+// TODO: Redo layout with grid to make this better.
 export function CreaturePresentation() {
-  const [creatureData, setCreatureData] = useState(generateCreatureData())
+  const [creature, setCreature] = useState(getNewCreature)
 
   function showNewCreature() {
-    setCreatureData(generateCreatureData());
+    setCreature(getNewCreature());
   }
 
   return (
     <div className="present-creature-wrapper" onClick={showNewCreature}>
-      <Creature size={250} creatureData={creatureData} />
+      <div className="creature-machine">
+        <div className="creature-machine-image">
+          <div className="creature-foreground">
+            <Creature size={150} creatureData={creature.data} />
+          </div>
+          <div className="creature-machine-image-background">
+            <TimeWidget size={700}/>
+          </div>
+        </div>
+        <div className="creature-machine-info">
+          {Object.keys(creature.attributes).map((name) => {
+            return <Attribute key={name} label={name} value={creature.attributes[name]} />
+          })}
+        </div>
+        <div className="creature-machine-title">Creature Finder</div>
+        <div className="creature-machine-detail-line line-1" />
+        <div className="creature-machine-detail-line line-2" />
+      </div>
     </div>
   );
 }
