4320fa58a6687bdfcc2b1af349360ce8ecb69f75
Clint Mullins <clintfmullins@gmail.com>
2018-11-30 18:25:42 -0500

    clean up work styles

--- src/features/nav-bar/component.js
+++ src/features/nav-bar/component.js
@@ -6,8 +6,8 @@ export function NavBar() {
   return (
     <div className="nav-bar">
       <Link className="profile nav-link-choice" to="/">clint.fm</Link>
-      <Link className="nav-link-choice-sub nav-link-choice" to="/work">work</Link>
-      <Link className="nav-link-choice-sub nav-link-choice" to="/play">play</Link>
+      <Link className="nav-link-choice-sub nav-link-choice choice-1" to="/work">work</Link>
+      <Link className="nav-link-choice-sub nav-link-choice choice-2" to="/play">play</Link>
     </div>
   );
 }
--- src/pages/work/component.js
+++ src/pages/work/component.js
@@ -4,57 +4,56 @@ import { TextAndPhoto } from '../../features/text-and-photo/component';
 
 export function Work() {
   return (
-    <div className="work">
-      <h1 className="work-title">Work</h1>
+    <div className="work-wrapper">
+      <div className="work">
+        <div className="intro">
+          <div>
+            Hi, my name is 
+            <br />
+            <span className="bold-it">Clint Mullins</span>.
+            <br />
+            <br />
+            I'm a <span className="bold-it">tech lead</span> with a <span className="bold-it">frontend focus</span>.
+            <div className="intro-hint">
+              Scroll if you have time, or just check out <a href="todo">LinkedIn</a>
+            </div>
+          </div>
+        </div>
 
-      <div className="intro">
-        Hi, my name is <span className="bold-it">Clint Mullins</span>.
-        <br />
-        I'm a <span className="bold-it">tech lead</span> with a <span className="bold-it">frontend focus</span>.
+        <div className="core-strengths">
+          <div>
+            <div className="core-strengths-header">Core Principles</div>
+            <div className="wordy-section">
+              <div className="wordy-section-title">Lead</div>
+              <div className="wordy-section-p">
+                My favorite leaders have been forward-thinking, empathetic, demanding,
+                full of ownership, and in the trenches with those they lead. I do my best to emulate
+                these traits and work on tight feedback loops with all teams I work with and within.
+              </div>
+            </div>
+            <div className="wordy-section">
+              <div className="wordy-section-title">Be pragmatic</div>
+              <div className="wordy-section-p">
+                At the end of the day, I work to make people's lives better. I want
+                to build great products for the user, I want to write clear specs for our
+                product team, and maintainable code for the engineer.
+                It is my job to make potentially large, ambiguous, complicated
+                requests into easy to use software.
+              </div>
+            </div>
+            <div className="wordy-section">
+              <div className="wordy-section-title">Be product obsessed</div>
+              <div className="wordy-section-p wordy-section-p-last">
+                If I don't understand why we are going to build something,
+                I ask questions until I do. This process often leads to
+                everyone understanding the product better. Any resulting changes
+                then happen during planning (inexpensive) as opposed to
+                after the product has been implemented (expensive).
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <h2>Core Strengths</h2>
-      <h3>1. Product focused</h3>
-      <p>
-        If I don't understand why I we are going to build something,
-        I start asking questions until I do. Doing this often leads to
-        everyone understanding the product better. Any resulting changes
-        this type then happen during specing (inexpensive) as opposed to
-        after the product has been built (expensive).
-      </p>
-
-      TODO: examples
-
-      <h3>2. Pragmatic</h3>
-      <p>
-        At the end of the day, I work to make people's life better. I want
-        to build great products for the user, I want to write clear specs for our
-        product team, and maintainable code for the engineer.
-        It is my job to make potentially large, ambiguous, complicated
-        requests into easy to use software.
-      </p>
-
-      TODO: examples
-
-      <h3>3. Leader</h3>
-      <p>
-        My favorite leaders have been forward-thinking, empathetic, demanding,
-        full of ownership, and in the trenches with me. I do my best to emulate
-        these traits each and every day.
-      </p>
-
-      TODO: examples
-
-      {/* <TextAndPhoto>
-        Core Strengths
-
-        <ul>
-          <li>Product focused</li>
-          <li>Process optimizer</li>
-          <li>Pragmatic engineer</li>
-          <li>Leader</li>
-        </ul>
-      </TextAndPhoto> */}
     </div>
   );
 }
