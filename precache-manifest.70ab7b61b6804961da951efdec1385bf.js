95d4d9c6686b7e8718abf93666da69b9ac6ccfd1
Clint Mullins <clintfmullins@gmail.com>
2019-02-03 17:57:04 -0800

    Updates

---
+++ precache-manifest.70ab7b61b6804961da951efdec1385bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad80a594dec3e62439e2",
+    "url": "/clintfm/static/js/runtime~main.ad80a594.js"
+  },
+  {
+    "revision": "9126eec8c7e0aaf36fe1",
+    "url": "/clintfm/static/js/main.9126eec8.chunk.js"
+  },
+  {
+    "revision": "aba01fe89405a3f4cc24",
+    "url": "/clintfm/static/js/1.aba01fe8.chunk.js"
+  },
+  {
+    "revision": "9126eec8c7e0aaf36fe1",
+    "url": "/clintfm/static/css/main.876b2e86.chunk.css"
+  },
+  {
+    "revision": "d5cfe87e0cb08e28bd9f3ac8f774b7ff",
+    "url": "/clintfm/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/clintfm/precache-manifest.12fdb18c7583da3319b0d8f7a37a02c7.js"
+  "/clintfm/precache-manifest.70ab7b61b6804961da951efdec1385bf.js"
 );
 
 workbox.clientsClaim();
