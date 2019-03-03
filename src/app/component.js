94a6d6a54817e2d66f3e2ee601fff09d70f8c2b1
Clint Mullins <clintfmullins@gmail.com>
2019-03-03 08:23:53 -0700

    Scroll to top react router

--- src/app/component.js
+++ src/app/component.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Homepage } from '../pages/homepage/component';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import { Projects } from '../pages/projects/component';
 import { Play } from '../pages/play/component';
 import { Work } from '../pages/work/component';
 import './styles.css';
 
+function ScrollToTopRaw({ location, children }) {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return children;
+}
+
+const ScrollToTop = withRouter(ScrollToTopRaw);
+
 export function App() {
   return (
-    <>
+    <ScrollToTop>
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/work' component={Work}/>
@@ -16,6 +26,6 @@ export function App() {
         <Route path='/play/' component={Projects}/>
       </Switch>
       {/* <Choice /> */}
-    </>
+    </ScrollToTop>
   );
 }
