771e411d5c3a5ba4f1f4a22452a8c08a8ff0f8f2
Clint Mullins <clintfmullins@gmail.com>
2019-04-21 16:41:56 -0700

    redid working, but not doing anything

--- src/lambda/redis.js
+++ src/lambda/redis.js
@@ -1,4 +1,18 @@
+const redis = require('redis');
+
+const [host, port] = process.env.REDIS_HOST.split(':');
+
+const client = redis.createClient({
+  port,
+  host,
+  password: process.env.REDIS_KEY
+});
+
 exports.handler = function(event, context, callback) {
+  console.log(event, context)
+
+  client.set("string key 22 2 2 2", "string val", redis.print);
+
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ msg: 'ok lets kill that secret' })
